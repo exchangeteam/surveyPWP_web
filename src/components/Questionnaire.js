9c236f6e8448f69166a749187ee97db8b69092f6
XCifer <13607960126@139.com>
2019-04-18 13:37:35 +0300

    Questionnaire page edit

--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -3,29 +3,35 @@ import {notification, Card} from 'antd'
 import axios from 'axios'
 import './Questionnaire.css'
 import 'antd/dist/antd.css'
-import { Button, Modal, Form, Input, Icon} from 'antd';
+import { Button, Modal, Form, Input, Icon, message} from 'antd';
 import api from './api'
 
 const { TextArea } = Input;
-const openNotificationWithIcon = (type) => {
+const openNotificationWithIcon = (type,msg,desc) => {
   notification[type]({
-    message: 'Error,',
-    description: 'there is an error in navigation, please go back!',
+    message: msg||'Error,',
+    description: desc||'there is an error in navigation, please go back!',
   });
 };
-
+const showMessage = (mode,msg) =>{
+  if (mode == "error")
+    message.error(msg)
+  else if (mode == "success")
+    message.success(msg)
+  else message.warning(msg)
+}
 const ModalForm = Form.create({name:"modal_form"})(
   class extends Component{
     render(){
       const {
-        visible, onCancel, onCreate, form,
+        visible, onCancel, onCreate, form, title
       } = this.props;
       const { getFieldDecorator } = form;
 
       return (
         <Modal
           visible={visible}
-          title="Add a question"
+          title={title||"Add a question"}
           okText="Create"
           onCancel={onCancel}
           onOk={onCreate}
@@ -33,7 +39,7 @@ const ModalForm = Form.create({name:"modal_form"})(
           <Form>
             <Form.Item label="Title">
               {getFieldDecorator('title', {
-                rules: [{ required: true, message: 'Please input the title of the question!' }],
+                rules: [{ required: true, message: ('Please input the title!') }],
               })(
                 <Input />
               )}
@@ -53,12 +59,24 @@ class Questionnaire extends Component {
     this.state={
       listLen:0,
       questions:[],
-      visible:false
+      visible:false,
+      modal_title:"Add a question"
     }
+  }
+  updateQuestionnaire = (location) =>{
+    let that = this
+    axios.get(location).then(res => {
+      console.log(res.data)
+      that.setState({
+          title:res.data.title,
+          id:res.data.id,
+          description:res.data.description
+      })
+    })
   }
   updateQuestions = (location) =>{
     let that = this
-    axios.get(location+"questions/").then(res => {
+    axios.get(location).then(res => {
       console.log(res.data)
       that.setState({
         listLen:res.data.items.length, 
@@ -67,7 +85,7 @@ class Questionnaire extends Component {
     })
   }
   showModal = () => {
-    this.setState({ visible: true });
+    this.setState({ visible: true, create:null });
   }
 
   handleCancel = () => {
@@ -83,15 +101,33 @@ class Questionnaire extends Component {
       console.log('Received values of form: ', values);
       form.resetFields();
       this.setState({ visible: false });
-      axios.post(that.props.location.state["location"]+"questions/",{"title":values["title"],"description":values["description"]}).then(res=>{
-        console.log(res)
-        axios.get(res.request.responseURL).then(res =>{
-          console.log("update questions")
-          that.setState({
-            questions:res.data.items
-          })
-        })    
-      })
+      let data = {"title":values["title"],"description":values["description"]}
+      if (this.state.create){
+        axios.put(this.state.targetLocation, data).then(res=>{
+          
+          let url = res.request.responseURL
+          console.log(url)
+          // url = url.substring(0,url.lastIndexOf("questions/"))
+          // console.log(url)
+          if (this.state.create === "editquestionnaire"){
+            this.updateQuestionnaire(url)
+          } 
+          else this.updateQuestions(url.substring(0,url.lastIndexOf("questions/")+10))
+          
+        })
+      }else{
+        axios.post(that.props.location.state["location"]+"questions/",data).then(res=>{
+          console.log(res)
+          // let url = res.request.responseURL         
+          // this.updateQuestions(url)
+          axios.get(res.request.responseURL).then(res =>{
+            console.log("update questions")
+            that.setState({
+              questions:res.data.items
+            })
+          })    
+        })
+      }
     });
   }
   saveFormRef = (formRef) => {
@@ -131,14 +167,38 @@ class Questionnaire extends Component {
     //   })
     // })
   }
-  handleDelete(location){
+  handleDelete(location,deleteType){
+    let that = this
     console.log("delete"+ location)
-    axios.delete(location,{headers:{"Accept":"application/vnd.mason+json"}}).then(res=>{
-      console.log(res)
+    axios.delete(location).then(res=>{
+      if(deleteType === "question"){
+        console.log(res)
+        let url = String(res.request.responseURL)
+        url = url.substring(0,url.lastIndexOf("questions/")+10)//a good way for string slice
+        console.log(url)
+        axios.get(url).then(res =>{
+          that.setState({
+            questions:res.data.items
+          })
+        }) 
+      }  
+    }).then(() => {
+      if (deleteType == "questionnaire")
+        this.props.history.push("/")
+      else if (deleteType == "question")
+        showMessage("success", deleteType+" is deleted!")
     })
+
   }
-  handleEdit(location){
+  handleEdit(location,editType){
     console.log("edit"+ location)
+    this.setState({
+      create:"edit"+editType,
+      targetLocation:location,
+      visible:true,
+      modal_title:"Edit a "+editType,
+      
+    })
   }
   render() {
     var questionnaireURL = this.props.location.state["location"]
@@ -150,8 +210,8 @@ class Questionnaire extends Component {
           title={q.title} 
           key={q.id} 
           extra={<div>
-                  <Icon onClick={this.handleDelete.bind(this,url)} type="delete" style={{paddingRight:"0.8em"}}/>
-                  <Icon onClick={this.handleEdit.bind(this,url)} type="edit" />
+                  <Icon onClick={this.handleDelete.bind(this,url,"question")} type="delete" style={{paddingRight:"0.8em"}}/>
+                  <Icon onClick={this.handleEdit.bind(this,url,"quesiton")} type="edit" />
                 </div>}
         >
           <p>{q.description ? ("Description:" + q.description ): "no description"}</p>
@@ -162,8 +222,8 @@ class Questionnaire extends Component {
       <div>
         <Card title={this.state.title} className='card' headStyle={{fontsize: "30px",fontweight: 500}}
           extra = {<div>
-                    <Icon onClick={this.handleDelete.bind(this,questionnaireURL)} type="delete" style={{paddingRight:"0.8em"}}/>
-                    <Icon onClick={this.handleEdit.bind(this,questionnaireURL)} type="edit" />
+                    <Icon onClick={this.handleDelete.bind(this,questionnaireURL,"questionnaire")} type="delete" style={{paddingRight:"0.8em"}}/>
+                    <Icon onClick={this.handleEdit.bind(this,questionnaireURL,"questionnaire")} type="edit" />
                   </div>} 
         >
           <p>{this.state.description ? ("Description:" + this.state.description ): "no description"}</p>
@@ -173,6 +233,7 @@ class Questionnaire extends Component {
           <div className="button_add">
             <Button type="primary" onClick={this.showModal}>Add a question</Button>
             <ModalForm
+              title = {this.state.modal_title}
               wrappedComponentRef={this.saveFormRef}
               visible={this.state.visible}
               onCancel={this.handleCancel}
